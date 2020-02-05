eb05501319cb6505a1bbec5a77d8d6dd4cd423c0
kindping <kindping@gmail.com>
2020-02-05 12:00:21 +0800

    Init

---
+++ src/app.js
@@ -0,0 +1,10 @@
+require('./bootstrap')
+
+new Vue({
+    el: '#app',
+    data() {
+        return {
+            title: 'Use Laravel-mix build vue, bootstrap 4, fontawesome 5 free, axios.',
+        }
+    },
+})
